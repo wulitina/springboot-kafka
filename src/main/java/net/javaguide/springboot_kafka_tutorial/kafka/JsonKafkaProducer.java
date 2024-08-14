eca22ba1dfd53df05e8120e2fdea5214f030cc91
wulitina <tinamazing.yan.xu@gmail.com>
2024-08-14 16:43:20 +0100

    create kafka producer to produce json message

---
+++ src/main/java/net/javaguide/springboot_kafka_tutorial/kafka/JsonKafkaProducer.java
@@ -0,0 +1,40 @@
+package net.javaguide.springboot_kafka_tutorial.kafka;
+
+import net.javaguide.springboot_kafka_tutorial.payload.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaProducer.class);
+
+    private KafkaTemplate<String, User> kafkaTemplate;
+
+
+    public JsonKafkaProducer(KafkaTemplate<String, User> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(User data){
+
+        LOGGER.info(String.format("Message sent -> %s",data.toString()));
+
+        Message<User> message = MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC,"javaguides")
+                .build();
+
+        kafkaTemplate.send(message);
+
+    }
+
+
+
+}
