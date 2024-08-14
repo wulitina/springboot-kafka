35c03d44f13d197d88272bf51536620b13d040b6
wulitina <tinamazing.yan.xu@gmail.com>
2024-08-14 11:46:51 +0100

    create kafka topic

---
+++ src/main/java/net/javaguide/springboot_kafka_tutorial/config/KafkaTopicConfig.java
@@ -0,0 +1,17 @@
+package net.javaguide.springboot_kafka_tutorial.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Bean
+    public NewTopic javaGuideTopic(){
+        return TopicBuilder.name("javaguides")
+                .build();
+    }
+
+}
+
